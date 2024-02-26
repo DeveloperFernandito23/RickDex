d635e261a212c70733e77d434d99cb2b118f14e6
Fernando Cervera Carrera <fcc3fernando@gmail.com>
2024-02-26 15:26:47 +0100

    add components

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import Constants from 'expo-constants'
+import { Text, View } from 'react-native'
+import FetchData from './FetchData.jsx';
+
+
+const Main = () =>{
+    return (
+        <View>
+            <FetchData />
+        </View>
+    )
+}
+
+export default Main
---
+++ src/components/fetchData.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList, Text, View, Image } from 'react-native';
+
+const FetchData = () => {
+    const [isLoading, setLoading] = useState(true);
+    const [dataFetch, setData] = useState([]);
+
+    const getMovies = async () => {
+        try {
+            const response = await fetch('https://rickandmortyapi.com/api/character');
+            const json = await response.json();
+            console.log(json);
+            setData(json.results);
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        getMovies();
+    }, []);
+
+    console.log(Object.values(dataFetch))
+
+    return (
+        <View style={{ flex: 1, padding: 24 }}>
+
+            <FlatList
+                data={Object.values(dataFetch)}
+                keyExtractor={({ id }) => id}
+                renderItem={({ item }) => (
+                    <View className="relative">
+                        <Text>
+                            {item.name}
+                        </Text>
+                        <Image
+                            className="w-10 h-10 opacity-95 "
+                            source={item.image}
+                        />
+                    </View>
+                )}
+            />
+
+        </View>
+    );
+};
+
+export default FetchData;
