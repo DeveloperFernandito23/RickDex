ef4c96066805901e112481c3e31dd9473011467a
Antonio Guillén <antonioguillengarcia123@gmail.com>
2024-03-02 15:22:46 +0100

    Refactoring Code
    Refactor Code and add inclusive language xd

--- navigation/MainStack.js
+++ navigation/MainStack.js
@@ -1,32 +1,32 @@
-import React from 'react'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { NavigationContainer } from '@react-navigation/native'
-import Main from '../src/components/Main.jsx'
-import MainDetails from '../src/components/MainDetails.jsx'
-import Favicon from 'react-favicon'
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import Main from '../src/components/Main.jsx';
+import MainDetails from '../src/components/MainDetails.jsx';
+import Favicon from 'react-favicon';
 
 
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
 
 export default function MainStack() {
-    return <NavigationContainer>
-        <Favicon url='../src/images/favicon.ico' />
+	return <NavigationContainer>
+		<Favicon url='../src/images/portal.png' />
 
-        <Stack.Navigator>
+		<Stack.Navigator>
 
-            <Stack.Screen
-                name='Main'
-                component={Main}
-                options={{
-                    headerShown: false
-                }}
-            />
+			<Stack.Screen
+				name='Main'
+				component={Main}
+				options={{
+					headerShown: false
+				}}
+			/>
 
-            <Stack.Screen
-                name='MainDetails'
-                component={MainDetails}
-            />
+			<Stack.Screen
+				name='MainDetails'
+				component={MainDetails}
+			/>
 
-        </Stack.Navigator>
-    </NavigationContainer>
+		</Stack.Navigator>
+	</NavigationContainer>;
 }
--- src/components/FetchCharacter.jsx
+++ src/components/FetchCharacter.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { Text, View, Image, SafeAreaView} from 'react-native';
+import { Text, View, Image, SafeAreaView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { GENDERS, STATES } from './FetchData';
+import { GENDERS } from './FetchData';
 
 const URL_API = 'https://rickandmortyapi.com/api/character/';
 
@@ -28,24 +28,38 @@ const FetchCharacter = ({ idCharacter }) => {
 		});
 	});
 
-	let originCharacter = character.origin?.name;
-	let locationCharacter = character.location?.name;
+	let characterOrigin = character.origin?.name;
+	let characterLocation = character.location?.name;
+	let characterGender = GENDERS[character.gender];
+	let characterSpecies = character.species;
+	let characterType = character.type;
+
+	let isFemale = characterGender === 'Femenino';
+
+	const STATES = {
+		Alive: isFemale ? 'Viva' : 'Vivo',
+		Dead: isFemale ? 'Muerta' : 'Muerto',
+		unknown: 'Desconocido'
+	};
+	
+	let characterStatus = STATES[character.status];
 
 	return (
 		<SafeAreaView className=' flex-1 bg-white'>
 			<View className='p-2 flex-column items-center max-w-s'>
 				<Image
-					className={'w-52 h-52 mr-8 rounded-xl'}
+					className={'w-80 h-72 mr-8 rounded-xl'}
 					style={{ margin: 0 }}
 					source={{
 						uri: character.image,
 					}}
 				/>
-				<Text className='font-bold text-l'>{character.species}</Text>
-				<Text className='font-bold text-l'>{character.type}</Text>
-				<Text className='font-bold text-l'>{GENDERS[character.gender]}</Text>
-				<Text className='font-bold text-l'>Origen: {originCharacter == 'unknown' ? 'Desconocido' : originCharacter}</Text>
-				<Text className='font-bold text-l'>Localización Actual: {locationCharacter == 'unknown' ? 'Desconocido' : locationCharacter}</Text>
+				<Text className='font-bold text-l'>{characterStatus}</Text>
+				<Text className='font-bold text-l'>{characterSpecies}</Text>
+				<Text className='font-bold text-l'>{characterType}</Text>
+				<Text className='font-bold text-l'>{characterGender}</Text>
+				<Text className='font-bold text-l'>Origen: {characterOrigin != 'unknown' ? characterOrigin : 'Desconocido'}</Text>
+				<Text className='font-bold text-l'>Localización Actual: {characterLocation != 'unknown' ? characterLocation : 'Desconocida'}</Text>
 			</View>
 		</SafeAreaView>
 	);
